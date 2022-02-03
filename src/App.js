2031f19df7475c08744c349b5d1cb49d477c7ffe
Zeizvant <oxanashvilizezva@gmail.com>
2022-02-03 16:02:00 +0400

    change 2 files (just experimenting)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Car(props){
+  return <li>I am a { props.brand }</li>;
 }
 
-export default App;
+export function Garage() {
+  const cars = ['Ford', 'BWM', 'Audi', 'Mercedes'];
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car brand={car}/>)}
+      </ul>
+    </>
+    
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {Garage} from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Garage />
   </React.StrictMode>,
   document.getElementById('root')
 );
